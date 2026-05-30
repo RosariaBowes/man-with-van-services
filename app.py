c4fe85934093d37a4bf3f3856859fd47570aa1b0
Rosie <BowesRT@cardiff.ac.uk>
2026-05-30 20:39:46 +0100

    Add robots.txt

--- app.py
+++ app.py
@@ -144,5 +144,9 @@ def google_verification():
 def sitemap():
     return app.send_static_file("sitemap.xml")
 
+@app.route("/robots.txt")
+def robots():
+    return app.send_static_file("robots.txt")
+
 if __name__ == "__main__":
     app.run(debug=True)
